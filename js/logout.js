1cceb0083391d588d0c5497560fee26cefee4e72
Max Buster <maxofboulder@gmail.com>
2016-11-05 18:22:13 -0500

    updated logout

--- js/logout.js
+++ js/logout.js
@@ -1,5 +1,8 @@
 function log_user_out() {
 	var sip_id = localStorage.getItem("sip");
+	if (sip_id === null) {
+		return;
+	}
 	var post_request = $.post(
 		"https://archhack2016.herokuapp.com/organizations/logout", // Url to post logout
 		{
@@ -8,11 +11,12 @@ function log_user_out() {
 	);
 	// Callback on post request success
 	post_request.done(function(data) {
-		// TODO remove sip and uid from local
-		// Redirect to login
+		localStorage.removeItem("sip"); // Remove cookies
+		localStorage.removeItem("uid");
+		window.location.assign("login.html"); // Redirect to login
 	});
 	// Callback on post request failure
 	post_request.fail(function() {
-		// TODO logout failed?
+		alert("Failed to log out. Please try again later.");
 	});
 }
