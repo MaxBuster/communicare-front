5bb6c41b76443736afdbb9bb9ab7c91094a0045b
Max Buster <maxofboulder@gmail.com>
2016-11-05 22:44:28 -0500

    added php for heroku

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+	<header>
+		<title>Home</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	</header>
+
+	<body>
+		<h1>Home</h1>
+
+		<p>About Us</p>p>
+	</body>
+
+</html>
