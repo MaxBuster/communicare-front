de1cded7a819acb239f3b459e2fa33440257af7e
Max Buster <maxofboulder@gmail.com>
2016-11-06 00:29:20 -0500

    styled pages

--- index.php
+++ index.php
@@ -1,15 +1,99 @@
 <!DOCTYPE html>
 <html>
+<title>CommuniCare</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inconsolata">
+<style>
+body, html {
+    height: 100%;
+    font-family: "Gill Sans", "Gill Sans MT", Calibri, sans-serif;
+}
+.bgimg {
+    background-position: center;
+    background-size: cover;
+    background-image: url("./img/front.jpg");
+    opacity: 0.65;
+    min-height: 80%;
+    max-width: 100%;
+    margin-top: 50px;
+    margin-left:-5px;
+}
+.menu {
+    display: none;
+}
+.w3-navbar {
+  background-color: #c64547;
+}
 
-	<header>
-		<title>Home</title>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-	</header>
+.w3-container {
+  background-color: #e9d0c9;
+  text-align: center;
 
-	<body>
-		<h1>Home</h1>
+}
 
-		<p>About Us</p>p>
-	</body>
 
+</style>
+<body>
+
+<!-- Navbar (sit on top) -->
+<div class="w3-top">
+  <ul class="w3-navbar w3-center w3-padding">
+
+    <li class="w3-left" style="width:20% !important"><a href="./"><font color=#f6eceb>HOME</font></a></li>
+    <li class="w3-left" style="width:20% !important"><a href="./mapviz.html"><font color=#f6eceb>MAP</font></a></li>
+    <li class="w3-left" style="width:20% !important"><a href="./newTent.html"><font color=#f6eceb>NEW LOCATION</font></a></li>
+    <li class="w3-left" style="width:20% !important"><a href="./register.html"><font color=#f6eceb>REGISTER</font></a></li>
+    <li class="w3-left" style="width:20% !important"><a href="./login.html"><font color=#f6eceb>SIGN IN</font></a></li>
+  </ul>
+</div>
+
+<!-- Header with image -->
+<header class="bgimg w3-display-container " id="home">
+  
+  <div class="w3-display-middle w3-center">
+    <span class="w3-text-white" style="font-size:90px"><br></span>
+  </div>
+  <div class="w3-display-bottomright w3-center w3-padding-xlarge">
+  <font color=#c64547>ARCHHACKS 2016</font>
+  
+  </div>
+</header>
+
+<!-- Add a background color and large text to the whole page -->
+<div class=c64547>
+
+<!-- About Container -->
+<div class="w3-container" id="about">
+  <div class="w3-content" style="max-width:700px">
+    <h5 class="w3-center w3-padding-48"><span class=" w3-wide w3-padding-left w3-padding-right"><font color=#c64547>MISSION STATEMENT</font></span></h5>
+    <p><font color=#f6eceb>A DATA VISUALIZATION APPLICATION THAT HELPS ORGANIZE RESOURCE ALLOCATION FOR LARGE-SCALE CRISIS SITUATIONS. A <B>COMMUNICATION </B>SERVICE FOR COMMUNITIES BOTH WITHIN ADN OUTSIDE AFFECTED ZONES, AND A CONNECTING FACTOR BETWEEN HELP AND <I>THOSE WHO NEED ME.</I></font></p>
+   
+   
+  </div>
+</div>
+
+<!-- Menu Container -->
+<div class="w3-container" id="menu">
+  <div class="w3-content" style="max-width:700px">
+ 
+    <h5 class="w3-center w3-padding-30"><span class="w3-tag w3-wide w3-padding-left w3-padding-right"></span></h5>
+  
+    
+  </div>
+</div>
+
+
+
+<!-- End page content -->
+</div>
+
+<!-- Footer -->
+<footer class="w3-center  w3-padding-48 w3-large">
+  <p><font color=#c64547><font size="2">POWERED BY HACKERS!</font></font></p>
+</footer>
+
+
+
+</body>
 </html>
